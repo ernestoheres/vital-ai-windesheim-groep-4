a3e6ca9d226fdac87312c9855d117dcf745221a2
ernestoheres <ernestoheres09@gmail.com>
2026-04-15 14:38:45 +0200

    oops

--- persoonlijke_notebooks/ernesto.py
+++ persoonlijke_notebooks/ernesto.py
@@ -24,6 +24,19 @@ def _(df):
 
 
 @app.cell
+def _(df):
+    # correlation matrix thats easy to look at 
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import matplotlib
+    corr_features = ['Hour', 'HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'Resp']
+    corr = df[corr_features].corr()
+    plt.figure(figsize=(120,100))
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    return
+
+
+@app.cell
 def _():
     return
 
