a6cdf29d6979b0fe2d11a13e83eef983bfdf1da3
Bataap <noah_boontje@hotmail.nl>
2026-05-04 18:35:37 +0200

    Moved SofaCalculator naar scepsis_prediction en toegevoegd aan init.py

--- src/scepsis_prediction/__init__.py
+++ src/scepsis_prediction/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for sepsis prediction code."""
 
 from . import evaluation
+from .SofaCalculator import SofaCalculator
 
-__all__ = ["evaluation"]
+__all__ = ["evaluation", "SofaCalculator"]
